2331918c39f61dfeed01c9f8fe8cb107f9fa3239
“Your <example@mail.com>
2023-03-10 19:18:57 +0500

    refactor answer

--- src/handlers.py
+++ src/handlers.py
@@ -1,22 +1,18 @@
 import asyncio
 
-from aiogram import Dispatcher, types, Bot
+from aiogram import Dispatcher, types
 
 import loggers
 from src.repo import IGPTRepo
-
-
-async def _ask_question(bot: Bot, tg_message: int, chat_id: int, gpt: IGPTRepo, question: str):
-    res = await asyncio.get_running_loop().run_in_executor(None, gpt.ask, question)
-    loggers.bot_logger.info(res)
-    await bot.send_message(chat_id, f"Ответ GPT: \n{res}", parse_mode='Markdown', reply_to_message_id=tg_message)
 
 
 async def echo(message: types.Message):
     """ Выполняется при любом сообщении """
     gpt: IGPTRepo = message.bot['repo_gpt']
     msg_id = await message.answer(f'Запрос \"{message.text}\" успешно задан в GPT')
-    await _ask_question(message.bot, msg_id.message_id, message.chat.id, gpt, message.text)
+    res = await asyncio.get_running_loop().run_in_executor(None, gpt.ask, message.text)
+    loggers.bot_logger.info(res)
+    await message.bot.send_message(message.chat.id, f"Ответ GPT: \n{res}", parse_mode='Markdown', reply_to_message_id=msg_id)
 
 
 async def start(message: types.Message):
@@ -27,3 +23,4 @@ async def start(message: types.Message):
 def register_user(dp: Dispatcher):
     dp.register_message_handler(start, commands=['start'])
     dp.register_message_handler(echo)
+
