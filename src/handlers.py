66035e8f50470537c81df4df4f069329cd8747d8
Rewale <44743547+Rewale@users.noreply.github.com>
2023-07-28 09:13:10 +0500

    Update handlers.py

--- src/handlers.py
+++ src/handlers.py
@@ -10,7 +10,10 @@ async def echo(message: types.Message):
     """ Выполняется при любом сообщении """
     gpt: IGPTRepo = message.bot['repo_gpt']
     msg_id = await message.answer(f'Запрос \"{message.text}\" успешно задан в GPT')
-    res = await asyncio.get_running_loop().run_in_executor(None, gpt.ask, message.text)
+    try:
+        res = await asyncio.get_running_loop().run_in_executor(None, gpt.ask, message.text)
+    except Exception as e:
+        res = str(e)
     loggers.bot_logger.info(res)
     await message.bot.send_message(message.chat.id, f"Ответ GPT: \n{res}", parse_mode='Markdown', reply_to_message_id=msg_id)
 
